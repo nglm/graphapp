b787b79c67d17715fdc84e0ff98a3cea0f046fdc
Natacha <nglm@protonmail.com>
2023-01-20 17:12:35 +0100

    add django.setup()

--- graphapp/graphapp/run_graphapp.py
+++ graphapp/graphapp/run_graphapp.py
@@ -8,5 +8,7 @@ from django import setup
 def start():
     settings.configure(DEBUG=False, ALLOWED_HOSTS = ['localhost', '127.0.0.1',])
     setup()
+    for name in dir(settings):
+        print(name, getattr(settings, name))
     management.call_command('runserver', '8080')
     webbrowser.open("http://127.0.0.1:8080/mjo/")
