d92b4949221391b6ec3c48255c3c87901d62daf1
Natacha <nglm@protonmail.com>
2023-01-20 17:04:37 +0100

    add django.setup()

--- graphapp/graphapp/run_graphapp.py
+++ graphapp/graphapp/run_graphapp.py
@@ -1,10 +1,12 @@
 import webbrowser
 from django.core import management
 from django.conf import settings
+from django import setup
 
 
 
 def start():
+    setup()
     settings.configure(DEBUG=False, ALLOWED_HOSTS = ['localhost', '127.0.0.1',])
     management.call_command('runserver', '8080')
     webbrowser.open("http://127.0.0.1:8080/mjo/")
