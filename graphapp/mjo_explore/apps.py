1d5f6abb8a2d5db82a778426873ac0316e6a9710
Natacha <nglm@protonmail.com>
2023-01-31 08:31:46 +0100

    making mjo clone app work

--- graphapp/mjo_explore/apps.py
+++ graphapp/mjo_explore/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MjoConfig(AppConfig):
+class MjoExploreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mjo'
+    name = 'mjo_explore'
