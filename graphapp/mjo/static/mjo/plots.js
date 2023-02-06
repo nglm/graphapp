b00d0a54402f3ff94cab8a2def141ad0cf7be298
Natacha <nglm@protonmail.com>
2023-02-06 14:24:55 +0100

    working arborescence .html .js .css

--- graphapp/mjo/static/mjo/plots.js
+++ graphapp/mjo/static/mjo/plots.js
@@ -1,14 +1,14 @@
 
-import { d3fy, d3fy_dict_of_arrays, d3fy_life_span } from "./mjo/preprocess.js";
+import { d3fy, d3fy_dict_of_arrays, d3fy_life_span } from "./preprocess.js";
 
 import {
     dimensions, setAxTitle, setFigTitle, setXLabel, setYLabel,
     init_fig, style_ticks, get_list_colors, fig_meteogram, fig_mjo, get_scalers,
     DIMS_mjo, set_fig_attrs
-} from "./mjo/figures.js"
-import { onEventClusterAux, onEventMemberAux, updateSliderValue} from "./mjo/interact.js";
+} from "./figures.js"
+import { onEventClusterAux, onEventMemberAux, updateSliderValue} from "./interact.js";
 
-import {range_rescale, sigmoid, linear} from "./mjo/utils.js"
+import {range_rescale, sigmoid, linear} from "./utils.js"
 
 export const PATH_SCRIPT = document.getElementById("main-script").getAttribute("path_script");
 const ROOT_DATA = 'data/'
